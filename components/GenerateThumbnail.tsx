ec3ea24c06d79eaa45a7366111e0e47432c918b9
ZaferCagatay <zafercagatayumut@gmail.com>
2024-12-13 11:11:21 +0300

    Fixed false error alert issue on Create Podcast page

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -118,7 +118,7 @@ const GenerateThumbnail = ({
           </div>
           <div className="w-full max-w-[200px]">
             <Button
-              type="submit"
+              type="button"
               className="text-16 bg-orange-1 py-4 font-bold text-white-1"
               onClick={generateImage}
             >
